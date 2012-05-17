a299311e6f260a290609c54e323c278c0e7326fb
Parsa <phossein@gmu.edu>
2012-05-17 10:14:32 -0400

    Updated fasta consolidation script to yield such results in an output fasta file.

--- src/gff_fasta/fasta_consolidate.py
+++ src/gff_fasta/fasta_consolidate.py
@@ -6,19 +6,38 @@ import optparse
 import os
 
 # function to parse multi-line fasta and yield only one single line each
-def parse_file(filename):
-	pass
+def parse_file(in_file, out_file):
+	results = {}
+	header = ''
+	for line in open(in_file):
+		line = str(line.strip())
+		if line.startswith('>'):
+			header = line
+			results[header] = ''
+		else:
+			results[header] += line
+	if len(results) > 0:
+		# save the consolidated results
+		out_handle = open(out_file, 'w')
+		for i in results:
+			out_handle.write(i+'\t'+ str(results[i])+'\n')
+			out_handle.flush()
+		out_handle.close()
+		print len(results), ' entries saved [OK]'
+	else:
+		print 'Either file is not fasta or it does not contain any entries'
 
 def main():
 	# create argument to parse user input file
 	parser = optparse.OptionParser()
 	parser.add_option("-i", "--input", help="Multi-line fasta file")
+	parser.add_option("-o", "--output", help="Consolidated output in_file")
 	(options, args) = parser.parse_args()
 	
-	# test to see if filename is valid
-	filename = options.input
-	if os.path.isfile(filename):
-		parse_file(filename)
+	# test to see if in_file is valid
+	in_file, out_file = options.input, options.output
+	if os.path.isfile(in_file):
+		parse_file(in_file, out_file)
 	
 
 if __name__ == '__main__':
