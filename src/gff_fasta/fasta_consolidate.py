5a1af5c258adb240a2f5df1ac7d750c4bb9c1f22
Parsa <phossein@gmu.edu>
2012-05-17 10:43:05 -0400

    Improved how consolidation works; splits fasta into chunks for easy parsing / sequence analysis

--- src/gff_fasta/fasta_consolidate.py
+++ src/gff_fasta/fasta_consolidate.py
@@ -5,8 +5,10 @@ A simple function to concat a multi-line fasta line into a single line
 import optparse
 import os
 
+N = 2000 # default chunk size; #/sequences per file
+
 # function to parse multi-line fasta and yield only one single line each
-def parse_file(in_file, out_file):
+def parse_file(in_file, out_dir):
 	results = {}
 	header = ''
 	for line in open(in_file):
@@ -17,27 +19,45 @@ def parse_file(in_file, out_file):
 		else:
 			results[header] += line
 	if len(results) > 0:
-		# save the consolidated results
-		out_handle = open(out_file, 'w')
-		for i in results:
-			out_handle.write(i+'\t'+ str(results[i])+'\n')
-			out_handle.flush()
-		out_handle.close()
+		partition(results, out_dir)
 		print len(results), ' entries saved [OK]'
 	else:
 		print 'Either file is not fasta or it does not contain any entries'
+
+def partition(results, out_dir):
+	# obtain keys of the dict for easy slicing
+	l = results.keys()
+	chunks = [l[i:i+N] for i in range(0, len(l), N)]
+	
+	if not os.path.exists(out_dir):
+		os.mkdir(out_dir)
+	
+	# iterate over each fragment and save it to its respective file
+	for frag_num, fragment in enumerate(chunks):
+		
+		# create output file to represent each fragment
+		out_handle = open(out_dir+'/fragment_' +str(frag_num)+'.fasta', 'w')
+		
+		# iterate over each sequence mapping in that fragment, save its seq
+		for a_key in fragment:
+			out_handle.write(a_key+'\n' + results[a_key]+'\n')
+			out_handle.flush()
+		out_handle.close()	
+	
+	print len(chunks), 'fragments'
+
 
 def main():
 	# create argument to parse user input file
 	parser = optparse.OptionParser()
 	parser.add_option("-i", "--input", help="Multi-line fasta file")
-	parser.add_option("-o", "--output", help="Consolidated output in_file")
+	parser.add_option("-o", "--output", help="Output directory")
 	(options, args) = parser.parse_args()
 	
 	# test to see if in_file is valid
-	in_file, out_file = options.input, options.output
+	in_file, out_dir = options.input, options.output
 	if os.path.isfile(in_file):
-		parse_file(in_file, out_file)
+		parse_file(in_file, out_dir)
 	
 
 if __name__ == '__main__':
