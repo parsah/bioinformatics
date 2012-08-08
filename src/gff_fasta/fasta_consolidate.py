661a8fb668282c3ce6975b05f88c60a8efd04a46
Parsa <phossein@gmu.edu>
2012-08-08 19:10:26 -0400

    added trivial but useful feature to stdout sequence length alongside sequence header

--- src/gff_fasta/fasta_consolidate.py
+++ src/gff_fasta/fasta_consolidate.py
@@ -8,7 +8,7 @@ import os
 N = 2000 # default chunk size; #/sequences per file
 
 # function to parse multi-line fasta and yield only one single line each
-def parse_file(in_file, out_dir):
+def parse_file(in_file, out_dir, show_stats):
 	results = {}
 	header = ''
 	for line in open(in_file):
@@ -19,8 +19,12 @@ def parse_file(in_file, out_dir):
 		else:
 			results[header] += line
 	if len(results) > 0:
-		partition(results, out_dir)
 		print len(results), ' entries saved [OK]'
+		if show_stats:
+			for entry in results:
+				print entry[1:]+'\t', len(results[entry])
+		else:
+			partition(results, out_dir)
 	else:
 		print 'Either file is not fasta or it does not contain any entries'
 
@@ -52,12 +56,14 @@ def main():
 	parser = optparse.OptionParser()
 	parser.add_option("-i", "--input", help="Multi-line fasta file")
 	parser.add_option("-o", "--output", help="Output directory")
+	parser.add_option('--stat', action='store_true', default=False,
+				help='Only stdout length of fasta entry [false]')
 	(options, args) = parser.parse_args()
 	
 	# test to see if in_file is valid
-	in_file, out_dir = options.input, options.output
+	in_file, out_dir, show_stat = options.input, options.output, options.stat
 	if os.path.isfile(in_file):
-		parse_file(in_file, out_dir)
+		parse_file(in_file, out_dir, show_stat)
 	
 
 if __name__ == '__main__':
