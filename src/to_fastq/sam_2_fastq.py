654c34646a4e0c52e8c74b14e722cf83a5b4df5a
Parsa <phossein@gmu.edu>
2012-08-20 18:29:43 -0400

    refactored flags which determine which chromosome(s) in SAM file to keep

--- src/to_fastq/sam_2_fastq.py
+++ src/to_fastq/sam_2_fastq.py
@@ -7,19 +7,19 @@ import argparse
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='SAM to fastq conversion script')
 	parser.add_argument('-in', help='Input SAM file [n/a]', metavar='', required=True)
-	parser.add_argument('-ignore', help='Ignore particular chromosome []', metavar='',
-					default='', type=str) # by default, ignore reads not mapping to reference	
+	parser.add_argument('-keep', help='Keep particular chromosome []', metavar='',
+					default='', type=str, required=True) # ignore not-mapped reads	
+	
 	args = vars(parser.parse_args()) # parse args and render it useful for manipulation 	
-	ignore_char, infile = args['ignore'], args['in'] # set user-provided params
+	ignore_char, infile = args['keep'], args['in'] # set user-provided params
 	
 	for line in open(infile): # parse file and iterate through its entries
 		line = line.strip().split('\t') # SAM files are tab-delimited
 		if len(line) > 9:
 			header, chrom, seq, qual = line[0], line[2], line[9], line[10]
-			# If no ignore character is provided, print entire enty. Also,
-			# if the ignore character does not equal the current chromosome
+			# If desired character is provided, print entire enty. Also,
+			# if this character does not equal the current chromosome
 			# character, ignore the entry (as it is desired to be ignored)
-			if ignore_char == '' or ignore_char != chrom:
+			if ignore_char in chrom:
 				fastq_str = '@'+header+'\n'+seq+'\n+\n'+qual
-				print chrom
-				
+				print fastq_str
