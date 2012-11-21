910397fb62b96cc7a9c54c63fc2f2842cb3e6e1d
parsah <phossein@gmu.edu>
2012-11-21 10:37:59 -0500

    refactoring of code to get GOs per uniprot accession.

--- src/conversion_scripts/uniprot_2_go.py
+++ src/conversion_scripts/uniprot_2_go.py
@@ -1,25 +1,23 @@
 """ 
 Given a set of uniprot accessions, fetch GO annotations for each accession.
-This logic is implemented using 'multiprocessing' and 'Elementree' 
+Since a uniprot accession can have multiple GO annotations, metadata can be
+supplied so as to aide in mapping GO results.
 """
 
 import argparse, urllib2
 from xml.etree import ElementTree 
 
-schema = '{http://uniprot.org/uniprot}'
-no_annot = '---' # represents uniprot accessions lacking a specific GO function 
-
 # Parses user-provided uniprot file	
-def load_in_file(filename):
+def load_input(filename):
 	handle = open(filename)
-	list_accns = []
-	for accn in handle:
-		accn = accn.strip().split('\t')
-		if len(accn) == 1: # if no metadata is provided, provide empty string
-			accn.append('')
-		if len(accn) > 0:
-			list_accns.append(accn)
-	return list_accns
+	accns = []
+	for entry in handle:
+		entry = entry.strip().split('\t')
+		if len(entry) == 1: # if no metadata is provided, provide empty string
+			entry.append('')
+		if len(entry) > 0:
+			accns.append(entry)
+	return accns
 
 # Cleanses output so that GO component, function and process are aligned
 def to_out(uniprot, metadata, component, function, process):
@@ -31,16 +29,16 @@ def to_out(uniprot, metadata, component, function, process):
 	func[0:len(function)] = function
 	proc[0:len(process)] = process
 	
-	for i in range(max_go):
+	for i in range(max_go): # print out all the Go annotations
 		print uniprot+'\t'+comp[i]+'\t'+ func[i]+'\t'+ proc[i]+'\t'+metadata
 
-def run_analysis(i, accn):
+def run_analysis(accn):
 	try:
-		uniprot, metadata = accn
+		uniprot, metadata = accn # only 2x columns allowed in input file
 		xml_fname = 'http://www.uniprot.org/uniprot/' + uniprot + '.xml'
 		data = urllib2.urlopen(xml_fname).read()
 		tree = ElementTree.XML(data)
-		func, proc, comp = set(), set(), set()
+		func, proc, comp = set(), set(), set() # stores Go ontologies
 		for node in tree.getiterator():
 			str_curr_tag = node.tag.replace('{http://uniprot.org/uniprot}', '')
 			# Each node = {'type': 'GO', 'id': 'GO:0005351', 'key': '17'}
@@ -56,26 +54,22 @@ def run_analysis(i, accn):
 				for child in children:
 					value = child.attrib['value']
 					if 'F:' in value:
-						#print '\t', value
 						func.add(value[2:]) # skip 'F:'
 					if 'P:' in value:
-						#print '\t', value
 						proc.add(value[2:]) # skip 'P:'
 					if 'C:' in value:
-						#print '\t', value
 						comp.add(value[2:]) # skip 'C:'
 		# there may be multiple GO components, functions or processes. In
 		# such a case, print each on a different line
 		to_out(uniprot, metadata, list(comp), list(func), list(proc))
-	except ElementTree.ParseError:
-		print accn+ '\t*** NOT FOUND ***'
+	except urllib2.HTTPError: # if an invalid URL is provided, print blanks
+		print uniprot + '\t' + metadata + '\t---\t---\t---'
 
 if __name__ == '__main__':
 	p = argparse.ArgumentParser(description='Uniprot to GO-annotations script')
 	p.add_argument('-in', metavar='FILE', required=True, 
 				help='File of uniprot accessions; line separated')
 	args = vars(p.parse_args())
-	uniprot_accns = load_in_file(filename=args['in'])
-	for i, accn in enumerate(uniprot_accns):
-		run_analysis(i, accn)
-			
+	accns = load_input(filename=args['in'])
+	for accn in accns: # get all GOs per uniprot accession
+		run_analysis(accn)
