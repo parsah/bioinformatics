727e536672a6a78a3b94c36ef7eb8bba97a270d9
parsah <phossein@gmu.edu>
2013-01-22 10:28:37 -0500

    revised how the query and target alignments are stored, and added behavior to remove unrequired metadata from maf+ files.

--- maf_scripts/mafplus_parser.py
+++ maf_scripts/mafplus_parser.py
@@ -1,7 +1,7 @@
 # A trivial MAF+ alignment parser; a file-format produced using the 
 # LASTZ alignment tool (Miller lab, PSU).
 
-import argparse
+import argparse, re
 
 # Represents an abstraction given a MAF+ entry  
 class MAFEntry():
@@ -16,22 +16,32 @@ class MAFEntry():
 
 # Represents a specific chromosomal region that the alignment pertains to. 
 class ChromosomeCoordinate():
-	def __init__(self):
-		self.align_len = 0
-		self.coordinate = 0
-		self.chrom = ''
-		self.strand = ''
-		self.seq = ''
+	def __init__(self, data):
+		self._data = data # references the actual data to be parsed
+		self.align_len = 0 # length of alignment
+		self.chrom_len = 0 # length of the actual chromosome
+		self.coordinate = 0 # region where alignment hits the chromosome
+		self.chrom = '' # chromosome
+		self.strand = '' # references sequence strand; + or -
+		self.seq = '' # references actual sequence masked or not
+		self._parse() # given the raw data, parse and assign to attributes
+	
+	# Each entry is space-delimited; split and assign to each attribute
+	def _parse(self):
+		self._data = re.sub(' +', ' ', self._data).split(' ')
+		self.chrom, self.coordinate = self._data[1], int(self._data[2])
+		self.align_len, self.strand = int(self._data[3]), self._data[4]
+		self.chrom_len, self.seq = int(self._data[5]), self._data[6]
+		self._data = None # raw data not needed since it is already processed 
 
 # Performs the bulk of the parsing.
 class MAFParser():
 	def __init__(self, fname):
 		self.fname = fname # references MAF+ filename
 		self.entries = [] # references all MAF entries
-		self._parse()
 	
 	# parse the provided MAF+ file
-	def _parse(self):
+	def parse(self):
 		data = open(self.fname)
 		maf_accn = None
 		for entry in data:
@@ -50,21 +60,57 @@ class MAFParser():
 				maf_accn.score = entry
 			elif entry.startswith('s '):
 				if not maf_accn.query: # set alignment coordinates to query
-					maf_accn.query = ChromosomeCoordinate()
+					maf_accn.query = ChromosomeCoordinate(entry)
 				else: # set alignment coordinates to target
-					maf_accn.target = ChromosomeCoordinate()
+					maf_accn.target = ChromosomeCoordinate(entry)
 		if len(self.entries) == 0:
 			raise IOError('Error: No MAF+ entries identified.')
 		else:
 			print('Success:', len(self.entries), 'parsed')
-					
+			self.remove_metadata() # remove metadata from all MAF+ states
+	
+	# Removes metadata from the MAF+ identity attribute
+	def _cleanse_identity(self, maf):
+		# A typical MAF+ entry looks like this: # identity=64/73 (87.7%)
+		# Only keep the percentage.
+		perc = maf.identity[maf.identity.find('(')+1: maf.identity.find(')')]
+		maf.identity = perc
+	
+	# Removes metadata from the MAF+ coverage attribute
+	def _cleanse_coverage(self, maf):
+		# A typical MAF+ entry looks like this: # coverage=62/366924 (0.0%)
+		# Only keep the percentage.
+		cov = maf.coverage[maf.coverage.find('(')+1: maf.coverage.find(')')]
+		maf.coverage = cov
+	
+	# Removes metadata from the MAF+ continuity attribute
+	def _cleanse_continuity(self, maf):
+		# A typical MAF+ entry looks like this: # coverage=62/366924 (0.0%)
+		# Only keep the percentage.
+		c = maf.continuity[maf.continuity.find('(')+1: maf.continuity.find(')')]
+		maf.continuity = c
+	
+	# Removes metadata from the MAF+ cigar attribute
+	def _cleanse_cigar(self, maf):
+		maf.cigar = maf.cigar.replace('# cigar=', '')
+		
+	# Removes metadata from the MAF+ score attribute
+	def _cleanse_score(self, maf):
+		maf.score = maf.score.replace('a score=', '')	
+		
 	# Each accession 
-	def _cleanse(self):
-		pass
+	def remove_metadata(self):
+		for maf_obj in self.entries:
+			self._cleanse_identity(maf_obj)
+			self._cleanse_coverage(maf_obj)
+			self._cleanse_continuity(maf_obj)
+			self._cleanse_cigar(maf_obj)
+			self._cleanse_score(maf_obj)
 	
 if __name__ == '__main__':
 	p = argparse.ArgumentParser()
 	p.add_argument('-i', metavar='FILE', required=True, # provide input file
 				help='MAF+ input file [na]', default=None)
 	args = vars(p.parse_args())
-	MAFParser(fname=args['i'])
+	parser = MAFParser(fname=args['i'])
+	parser.parse()
