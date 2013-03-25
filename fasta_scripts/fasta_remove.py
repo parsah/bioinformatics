fe076ab79a27dcf7a2b72bbd7e38d5fa07afbc3e
parsah <phossein@gmu.edu>
2013-03-25 11:03:31 -0400

    added simple script to filter a FASTA file given a desired header pattern.

---
+++ fasta_scripts/fasta_remove.py
@@ -0,0 +1,25 @@
+import Bio
+import sys
+import argparse
+
+from Bio import SeqIO
+
+def filter_seqs(fname, pattern):
+    # Read FASTA files and see if pattern is found in their headers.
+    fasta_records = SeqIO.parse(fname, 'fasta')
+    for record in fasta_records:
+        if pattern in record.description: # print if pattern in the header.
+            sys.stdout.write(">"+record.description+"\n"+\
+                             str(record.seq)+"\n")
+            sys.stdout.flush()
+    sys.stdout.close()
+    
+
+# A simple script to remove FASTA entries containing a specific substring.
+parser = argparse.ArgumentParser()
+parser.add_argument('-in', metavar='FASTA', required=True,
+                    help='Input FASTA file [na]')
+parser.add_argument('-pattern', metavar='STR', required=True,
+                    help='Substring for which to keep FASTA entries [na]')
+args = vars(parser.parse_args())
+filter_seqs(fname=args['in'], pattern=args['pattern'])
