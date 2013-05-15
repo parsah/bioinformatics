39a4884121b6dc493c52d4fa2b3354aaff526638
parsah <phossein@gmu.edu>
2013-05-15 15:07:07 -0400

    added full PWM cleansing so as to provide Marina-compatible PWMs

--- pwm_scripts/pwm_cleanser.py
+++ pwm_scripts/pwm_cleanser.py
@@ -11,9 +11,25 @@ def parse(fname):
     @param fname: Input filename referencing the PWM.
     @return: multi-dimensional array containing PWM values.
     '''
+    pwm_contents = []
     for line in open(fname):
-        line = line.strip().split(' ')
-        print(line)
+        line = line.strip().split(' ') # PWMs are space-delimited
+        # multiply floats by 100 to represent as percentage.
+        line = [round(float(i)*100, 2) for i in line]
+        pwm_contents.append(line)
+    return pwm_contents
+
+def cleanse(pwm_data):
+    ''' 
+    Transposes a 2D matrix.
+    @param pwm_data: Multi-dimensional matrix of real-numbers
+    @return: transposed matrix.
+    '''
+    tranposed = list(zip(*pwm_data))
+    row_names = ['A', 'C', 'G', 'T']
+    for num, row in enumerate(tranposed):
+        row = [str(i) for i in row] # cast float back to string for writing
+        print(row_names[num]+ ' ' + ' '.join(row))
 
 if __name__ == '__main__':
     desc = """ 
@@ -24,5 +40,6 @@ if __name__ == '__main__':
     parser.add_argument('-pwm', required=True, metavar='PWM',
                         help='PWM file; 4x columns wide [na]')
     args = vars(parser.parse_args())
-    parse(fname=args['pwm'])
+    pwm_data = parse(fname=args['pwm'])
+    pwm_data = cleanse(pwm_data) # streams data out to standard-out
     
