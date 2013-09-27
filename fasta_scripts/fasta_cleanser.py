e9ae97b4ecebbc1341e7b66c3421afd4d861ad59
parsah <phossein@gmu.edu>
2013-09-27 16:21:53 -0400

    revised FASTA cleanser script to remove non-DNA bases rather than user-provided bases.

--- fasta_scripts/fasta_cleanser.py
+++ fasta_scripts/fasta_cleanser.py
@@ -3,24 +3,35 @@ Script which removed FASTA entries with user-provided invalid characters.
 '''
 
 import argparse
+import re
 from Bio import SeqIO
 
-def run(fname, chars):
+def display_seq(header, seq):
+    ''' 
+    Prints sequence header and sequence information in FASTA format
+    @param header: FASTA header
+    @param seq: FASTA sequence
+    '''
+    print('>' + header + '\n' + seq)
+
+def run(fname, is_filter):
     entries = SeqIO.parse(fname, 'fasta')
     for i in entries:
-        is_clean = True
-        for c in chars:
-            if c in i.seq:
-                is_clean = False
-        if is_clean in i.description:
-            print('>' + i.description + '\n' + str(i.seq))
+        seq = str(i.seq).upper()
+        num_a, num_t, num_n = seq.count('A'), seq.count('T'), seq.count('N')
+        num_g, num_c = seq.count('G'), seq.count('C')
+        if (num_a + num_c + num_g + num_t + num_n) == len(seq):
+            display_seq(header = i.description, seq = seq) # clean sequence
+        elif is_filter:
+            corrected_seq = re.sub('[^ATGCN]', 'N', seq) # filter bases
+            display_seq(header = i.description, seq = corrected_seq)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', required=True, metavar='FASTA',
                         help='FASTA file [na]')
-    parser.add_argument('-chars', required=True, metavar='', nargs='+',
-                        help='List of characters to filter [na]')
+    parser.add_argument('--filter', action='store_const', const=True,
+                        default=False, help='Filter non-ATGCN bases [true]')
     args = vars(parser.parse_args())
-    run(fname = args['i'], chars = args['chars'])
+    run(fname = args['i'], is_filter = args['filter'])
     
