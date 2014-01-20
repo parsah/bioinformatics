6e7350e2001c318ef0626d6ccdc23e9927ec4b6f
parsah <parsa.hosseini@nih.gov>
2014-01-20 00:16:12 -0500

    helpful Homebrew script to help install useful apps.

---
+++ install_brew_libs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Helpful Homebrew script for installing commonly-used Mac OSX apps.
+
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" # download brew
+brew doctor
+
+brew install git
+brew install homebrew/versions/gcc49
+brew install python3
