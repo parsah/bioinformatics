de89bc829160f4efc6ca686805f9f06a02af25a5
parsah <parsa.hosseini@nih.gov>
2014-01-27 11:38:01 -0500

    new script that iteratively summarizes PWMs originally in MEME format.

---
+++ tfbs/meme_summary.py
@@ -0,0 +1,92 @@
+''' 
+Classes that parse and quantify PWMs used by the MEME Suite.
+Such classes first parse a MEME PWM file, with each PWM being modeled as
+an object of type MEMEPWMRecord. Each record of this type can have its
+information content computed; useful in analyses where you wish to determine
+which PWM has the lowest information given another. This very conclusion helps
+to filter-out low-information PWMs, ultimately helping to reduce the 
+feature-space during TFBS analysis.
+'''
+
+import argparse
+import numpy
+import re
+
+class MEMEPWMRecord():
+    ''' 
+    Models an individual PWM originally presented in the MEME format.
+    Each object references its original weight matrix and a corresponding 
+    motif name.
+    '''
+    def __init__(self, name):
+        ''' 
+        Build a skeleton PWM record.
+        @param name: String referencing the respective object.
+        '''
+        self.matrix = []
+        self.name = name
+    
+    def set_pwm(self, m):
+        ''' 
+        Set the actual matrix referencing this record object. This matrix
+        must be originally from a MEME PWM file; see file format for details.
+        @param m: Multi-dimensional matrix of probabilities.
+        '''
+        assert m.shape[1] == 4 # assert 4 rows (A, T, G, C) are added
+        self.matrix = m
+    
+    def get_information(self):
+        pass
+
+class PWMContrastFactory():
+    def __init__(self, pwms):
+        assert isinstance(pwms, list) # assert list is provided
+        assert all([isinstance(p, MEMEPWMRecord) for p in pwms]) # must be PWM
+        self.pwms = pwms
+        
+    def run(self):
+        ''' 
+        Contrast PWMs in a pairwise manner; comparing their Shanon entropy.
+        '''
+
+class MEMEPWMParser():
+    def __init__(self, f):
+        self.f = f
+        self.records = []
+        
+    def parse(self):
+        entries = open(self.f)
+        hit_str = '' # save all results to string for easier processing
+        for line in entries:
+            line = line.strip()
+            # match entries beginning with MOTIF and floating points 
+            match = re.findall('^MOTIF|^[0-9]', line)
+            if match:
+                hit_str += line + '\n'
+        hit_str = hit_str.split('MOTIF')[1:] # first hit is empty
+        
+        # iterate over each motif and save into an array
+        for i in hit_str:
+            i = i.splitlines()
+            motif_id = i[0].strip()
+            probs = i[1: ]
+            matrix = []
+            for row in probs:
+                matrix.append([float(x) for x in row.split()])
+            rec = MEMEPWMRecord(motif_id)
+            rec.set_pwm(numpy.array(matrix)) # set the respective PWM
+            self.records.append(rec)
+            
+    def get_records(self):
+        return self.records        
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i',metavar='FILE', required=True,
+                        help='PWMs; MEME format [req]')
+    args = vars(parser.parse_args())
+    meme_parser = MEMEPWMParser(f = args['i'])
+    meme_parser.parse()
+    factory = PWMContrastFactory(pwms = meme_parser.get_records())
+    
+    
