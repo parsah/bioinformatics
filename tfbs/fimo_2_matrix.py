f35630be94e96f98a5003ea8dfdabc1e65828767
parsah <parsa.hosseini@nih.gov>
2014-01-28 13:07:05 -0500

    improved commenting of classes used to build FIMO count-matrix.

--- tfbs/fimo_2_matrix.py
+++ tfbs/fimo_2_matrix.py
@@ -1,24 +1,41 @@
 '''
-Creates a count-matrix given FIMO output using its --text format.
+A collection of classes which collectively derive a count-matrix given
+a baseline and query FIMO output file.
 '''
 
 import argparse
 import pandas
-import tempfile
 from pwm_info_content import MEMEPWMParser
 from collections import OrderedDict
 
-COLNAME_SEQ = 'Sequence'
-COLNAME_TARGET = 'Target'
-
 class FIMOMatrixBuilder():
+    ''' 
+    Constructs a count-matrix given a baseline and query FIMO
+    output file. Such a file must be the result of running FIMO in
+    --text mode.
+    '''
+    COLNAME_SEQ = 'Sequence'
+    COLNAME_TARGET = 'Target'
+    
     def __init__(self, frs_a, frs_b):
+        ''' 
+        Constructs a bare-bones count-matrix.
+        @param control_frs: Control (baseline) FIMOResultSet object.
+        @param query_frs: Query FIMOResultSet object.
+        '''
         self.control_frs = frs_a
         self.query_frs = frs_b
-        self.df = None
-        self.build_skeleton()
+        self.df = None # references the count-matrix (aka. data-frame)
+        self.filter_cont = None # references control FIMOFilter object
+        self.filter_query = None # references query FIMOFilter object
+        self._build_skeleton()
         
-    def build_skeleton(self):
+    def _build_skeleton(self):
+        ''' 
+        Builds the row and column layout for the current matrix object.
+        This resultant matrix is empty and is yet to be populated with
+        actual count-data.
+        '''
         pwms = set() # ordering is not important
         all_seqs = []
         
@@ -34,21 +51,41 @@ class FIMOMatrixBuilder():
             all_seqs.extend(seqs)
         
         # wrap counts in a DataFrame
-        m = OrderedDict({COLNAME_SEQ: all_seqs})
+        m = OrderedDict({FIMOMatrixBuilder.COLNAME_SEQ: all_seqs})
         m.update({pwm: [0] * len(all_seqs) for pwm in pwms})
-        m.update({COLNAME_TARGET: ['None'] * len(all_seqs)})
+        m.update({FIMOMatrixBuilder.COLNAME_TARGET: ['None'] * len(all_seqs)})
         self.df = pandas.DataFrame(m, index=all_seqs) # captures count-matrix
     
+    def set_filters(self, filter_cont, filter_query):
+        ''' 
+        Sets FIMOFilter objects which will be used to remove 
+        redundant PWMs from the count-matrix.
+        @param filter_cont: Control FIMOFilter object.
+        @param filter_query: Query FIMOFilter object. 
+        '''
+        assert isinstance(filter_cont, FIMOFilter)
+        assert isinstance(filter_query, FIMOFilter)
+        self.filter_cont = filter_cont
+        self.filter_query = filter_query
+    
     def write(self, csv):
-        for i, seq in enumerate(self.df[COLNAME_SEQ]):
-            seq = self.df[COLNAME_SEQ][i] 
+        ''' 
+        Writes the current count-matrix to a user-provided CSV file.
+        @param csv: Output filename.
+        '''
+        for i, seq in enumerate(self.df[FIMOMatrixBuilder.COLNAME_SEQ]):
+            seq = self.df[FIMOMatrixBuilder.COLNAME_SEQ][i] 
             # replace unique delimiters so both control and query sequences
             # share possible sub-sequences., i.e. match.chr1.111.553
             # is related to chr1.111.553, and so on.
-            self.df[COLNAME_SEQ][i] = seq.replace(':', '.').replace('-', '.')
+            self.df[FIMOMatrixBuilder.COLNAME_SEQ][i] = seq.replace(':', '.').replace('-', '.')
         self.df.to_csv(csv)
     
     def populate(self):
+        ''' 
+        Adds counts to current matrix object, a process necessary to
+        ultimately build a functional object.
+        '''
         assert self.df # data-frame must be valid
         for i, dataset in enumerate([self.control_frs.f, self.query_frs.f]):
             for linenum, line in enumerate(open(dataset)):
@@ -56,30 +93,59 @@ class FIMOMatrixBuilder():
                     line = line.strip().split()
                     pwm, seq = line[0: 2]
                     self.df[pwm][seq] += 1 # increment sequence-PWM count
-                    self.df[COLNAME_TARGET][seq] = i
+                    self.df[FIMOMatrixBuilder.COLNAME_TARGET][seq] = i
+        if self.filter_cont and self.filter_query: # if valid filters provided
+            pwms_del = set(self.filter_cont.pwms_remove).union(self.filter_query.pwms_remove)
+            self.df.drop(list(pwms_del), axis=1) # delete PWMs flagged for removal
 
 class FIMOFilter():
+    ''' 
+    Oftentimes, a PWM may map to the same sequence indices as another PWM.
+    In such instances, there is no need to keep both PWMs since the 
+    mapping dynamics of the former can be captured by the latter. By
+    filtering for such redundancy, the feature space is reduced; allowing
+    for easier feature discrimination and user interpretation.
+    '''
+    
     def __init__(self):
+        ''' 
+        Creates a filter object that captures redundant PWMs. 
+        '''
         self.pwm_info = None # references PWM information content
         self.resultset = None # FIMO result-set
-        self.pwms_remove = set()
+        self.pwms_remove = set() # references PWMs to be removed.
         
     def set_resultset(self, rs):
+        ''' 
+        Set the FIMOResultSet object utilized by this object.
+        @param rs: FIMOResultSet object.
+        '''
         assert isinstance(rs, FIMOResultSet)
         self.resultset = rs
     
     def set_meme_pwms(self, pwms):
+        ''' 
+        Sets the MEME-format PWMs used to deduce whether one PWM is
+        redundant given another. The information content per PWM is
+        used as a factor in such determination.
+        @param pwms: List of MEMEPWMRecord objects.
+        '''
         self.pwm_info = pwms    
     
     def as_fimo_handle(self):
+        ''' 
+        Following filtering, the filtered-file is to be saved so that
+        it can be fed back into the analysis pipeline so that a count-matrix
+        can be built around this high-quality data-set. 
+        '''
         fname = self.resultset.f+'.filtered'
         out_handle = open(fname, 'w')
-        out_handle.write('#pattern sequence start\n')
+        out_handle.write('#pattern sequence start\n') # headers.
         out_handle.flush()
         for seq in self.resultset.mapping:
             seq_hits = self.resultset.mapping[seq]
             for motif in seq_hits:
-                for hit in seq_hits[motif]:
+                for hit in seq_hits[motif]: # write each entry to file.
                     out_handle.write(motif + ' ' + seq + ' ' + str(hit) + '\n')
                     out_handle.flush()
         out_handle.close()
@@ -112,16 +178,37 @@ class FIMOFilter():
                             pwm_b_info = self.pwm_info[pwm_b]
                             if pwm_a_info >= pwm_b_info: # select worst info
                                 self.pwms_remove.add(pwm_a)
+                                print('[INFO]',pwm_a, 'is flagged for removal')
                             else:
                                 self.pwms_remove.add(pwm_b)
+                                print('[INFO]',pwm_b, 'is flagged for removal')
 
 class FIMOResultSet():
+    ''' 
+    Models contents of a FIMO output file produced from running fimo in 
+    --text mode. There may be instances in such a file whereby a PWM can
+    hit the same sequence index multiple times. In such an instance, there
+    may be a differing p-value. This class only models one index and not
+    all such indices.
+    '''
     def __init__(self, f):
+        ''' 
+        Builds an object of type FIMOResultSet strictly based on a 
+        user-provided FIMO --text mode input file.
+        @param f: Input FIMO file.
+        @param mapping: Hash of sequences and their mapped PWMs.
+        @param pwms: Collection of PWMs mapping to file.
+        '''
         self.f = f
         self.mapping = {}
         self.pwms = set()
     
     def parse(self):
+        ''' 
+        Parses a FIMO file so as to keep track of what PWMs mapped
+        to what sequences, and how many unique PWMs there are in the
+        input file.
+        '''
         for num, line in enumerate(open(self.f)):
             line = line.strip().split()
             if num != 0:
@@ -133,18 +220,14 @@ class FIMOResultSet():
                 self.mapping[seq][motif_id].add(int(hit))
                 self.pwms.add(motif_id)
     
-    @staticmethod
-    def fill_empty(query, control):
-        assert isinstance(query, FIMOResultSet)
-        assert isinstance(control, FIMOResultSet)
-        shared_pwms = set(control.get_pwms()).union(query.get_pwms())
-        for dataset in [query, control]:
-            for seq in dataset.mapping:
-                for pwm in shared_pwms:
-                    if pwm not in dataset.mapping[seq]:
-                        dataset.mapping[seq][pwm] = set()
-    
     def to_pwm_counts(self):
+        ''' 
+        Unlike the default mapping, this behavior enables derivation of
+        a hash that references where each PWM maps across the entire 
+        sequence. This function is useful in instances in-which you wish
+        to investigate similarity between where PWM x maps with PWM y.
+        @return: dictionary; key: PWM, value: set of mapped indices.
+        '''
         counts = {}
         for seq in self.mapping:
             for motif in self.mapping[seq]:
@@ -153,13 +236,6 @@ class FIMOResultSet():
                 # update map with indices of where PWM hits.
                 counts[motif].update(self.mapping[seq][motif])
         return counts
-    
-    def hit_count(self):
-        num = 0
-        for seq in self.mapping:
-            for motif in self.mapping[seq]:
-                num += len(self.mapping[seq][motif])
-        return num
     
     def to_sequence_counts(self):
         return self.mapping # by-default, sequences reference their PWM
@@ -181,12 +257,14 @@ if __name__ == '__main__':
     cont_fname, query_fname = args['control'], args['query']
     
     try:
+        # === parse the user-provided FIMO files === 
         control = FIMOResultSet(f = args['control']) # parse the query and control file.
         query = FIMOResultSet(f = args['query']) # parse the query and control file.
         control.parse()
         query.parse()
-        FIMOResultSet.fill_empty(query, control)
-            
+        filter_query, filter_cont = None, None # reference FIMO filteration
+        
+        # === perform FIMO filtering on query, control if requested ===
         if args['m']:
             meme_pwms = MEMEPWMParser(args['m']).get_records() # PWM info. content
             filter_cont = FIMOFilter()
@@ -194,17 +272,20 @@ if __name__ == '__main__':
             filter_cont.set_meme_pwms(meme_pwms)
             filter_cont.filter()
 
-            filter_query = FIMOFilter()
+            filter_query = FIMOFilter() # filter query file.
             filter_query.set_resultset(query)
             filter_query.set_meme_pwms(meme_pwms)
             filter_query.filter()
 
+            # === parse the FIMO-filtered file ===
             control = FIMOResultSet(filter_cont.as_fimo_handle())
             query = FIMOResultSet(filter_query.as_fimo_handle())
             control.parse()
             query.parse()
-            FIMOResultSet.fill_empty(query, control)
+        
+        # === build a count-matrix and save results to file === 
         fmb = FIMOMatrixBuilder(control, query)
+        fmb.set_filters(filter_cont, filter_query) # set the filters
         fmb.populate()
         fmb.write(csv = args['csv'])
             
