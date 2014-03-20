1378b9eecf60d994b87374285b8af52014d54243
parsah <parsa.hosseini@nih.gov>
2014-03-20 13:59:10 -0400

    reverting standardization back to Z-scores.

--- lasso/lasso.R
+++ lasso/lasso.R
@@ -249,17 +249,9 @@ getWeights <- function(fit.cv, scale.weights=T) {
 
   m <- as.matrix(coef(fit.cv, s=min(fit.cv$lambda)))
   if (scale.weights) { # standardize LASSO weights, if sought.
-    weights.min <- min(m)
-    weights.max <- max(m)
-    for (i in 1: length(m)) {
-      val <- m[i, 1] # retrieve the current value
-      if (val < 0) { # normalize weights less than zero
-        m[i, 1] <- -1 * ( 1 - ( ( val - weights.min ) / -weights.min ) )
-      }
-      else { # normalize weights greater-than or equal to zero
-        m[i, 1] <- val / weights.max # maximum weight is set to 1.0
-      }
-    }
+    avg <- mean(m)
+    stdev <- sd(m)
+    m <- ((m - avg) / stdev)
   }
   
   m <- as.matrix(m[order(row.names(m)), ]) # order weights by row-name
