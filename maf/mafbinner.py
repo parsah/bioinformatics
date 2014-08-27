6caeecf4e723e0fbe53da98a9e7140a59da762f4
parsah <parsa.hosseini@nih.gov>
2014-08-27 13:14:35 -0400

    MAF mapping is performed in a concurrent fashion.

--- maf/mafbinner.py
+++ maf/mafbinner.py
@@ -4,12 +4,14 @@ and corresponding organisms are extracted.
 """
 
 import sys
+from concurrent.futures import ProcessPoolExecutor
 
 def __maf_to_features(line):
-    line = filter(None, line.split(' '))
+    line = list(filter(None, line.split(' ')))
     position = int(line[2])
     organism, chromosome = line[1].split('.')[0: 2]
-    return (organism, chromosome, position)
+    return organism, chromosome, position
+
 
 def parse_maf(maf):
     hg_chromosome = ''
@@ -24,25 +26,26 @@ def parse_maf(maf):
                     contents[hg_chromosome] = {}
                 contents[hg_chromosome][hg_position] = set([hg19])  # each element is found in hg19 by default
             else:
-                if 'scaffold' not in line:
-                    organism, org_chromosome, org_position = __maf_to_features(line)
-                    contents[hg_chromosome][hg_position].add(organism)
+                # if 'scaffold' not in line:
+                organism, org_chromosome, org_position = __maf_to_features(line)
+                contents[hg_chromosome][hg_position].add(organism)
     return contents
 
-def map_intervals(bed, maf):
-    for bed_entry in open(bed):
-        bed_entry = bed_entry.strip().split('\t')
-        bed_chromosome, bed_start, bed_end = bed_entry[0], int(bed_entry[1]), int(bed_entry[2])
-        if bed_chromosome in maf:
-            positions = maf[bed_chromosome]
-            organisms = set()
-            for position in positions:
-                if bed_start-(bed_end-bed_start) < position < bed_end+(bed_end-bed_start):
-                    organisms.update(maf[bed_chromosome][position])
-            if organisms:
-                for organism in organisms:
-                    print str(bed_chromosome) + '\t' + str(bed_start) + '\t' + str(bed_end) + '\t' + organism
 
+def map_intervals(chromosome, start, end, maf):
+        organisms = set()
+        diff = end - start
+        for position in maf:
+            if (start - diff) < position < (end + diff):
+                organisms.update(maf[position])
+        return chromosome, start, end, organisms
+
+
+def cb(obj):
+    chromosome, start, end, organisms = obj.result()
+    if len(organisms) > 0:
+        for organism in organisms:
+            print(chromosome + '\t' + str(start) + '\t' + str(end) + '\t' + organism)
 
 if __name__ == '__main__':
     try:
@@ -50,8 +53,14 @@ if __name__ == '__main__':
             print('Arg1 => BED file')
             print('Arg2 => MAF file')
         else:
+            executor = ProcessPoolExecutor(8)
             maf_data = parse_maf(sys.argv[2])
-            map_intervals(sys.argv[1], maf_data)
+            for bed_entry in open(sys.argv[1]):
+                bed_entry = bed_entry.strip().split('\t')
+                bed_chromosome, bed_start, bed_end = bed_entry[0], int(bed_entry[1]), int(bed_entry[2])
+                if bed_chromosome in maf_data:
+                    future = executor.submit(map_intervals, bed_chromosome, bed_start, bed_end, maf_data[bed_chromosome])
+                    future.add_done_callback(cb)
     except KeyboardInterrupt:
         print()
     except KeyError as e:
